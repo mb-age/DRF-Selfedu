af66481c59f01a131e200a4b3c271124e84787e6
mb-age <i.m.angel.777@gmail.com>
2022-04-16 19:16:24 +0200

    Initial, models created, overwriten get and post methods in views, simple serializer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DRF-Selfedu
 Making a site about famous women based on Django Rest Framework
+
+Based on Selfedu tutorial
---
+++ women/views.py
@@ -0,0 +1,44 @@
+from django.forms import model_to_dict
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Women
+from .serializers import WomenSerializer
+
+
+class WomenAPIView_aaa(generics.ListAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+
+class WomenAPIView_bbb(APIView):
+    def get(self, request):
+        return Response({'имя':'Яна'})
+    def post(self, request):
+        return Response({'имя': 'Янка'})
+
+
+class WomenAPIView(APIView):
+    def get(self, request):
+        lst = Women.objects.all().values() # потому что без values это queryset, а нам нужен набор значений
+        return Response({'посты':list(lst)})
+
+    def post(self, request):
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            category_id=request.data['category_id'], # почему-то с "_id", без этого не работает
+        )
+        return Response({'пост': model_to_dict(post_new)})
+
+
+
+
+
+
+
+
+
+
